74022aee44482f00756ee1b008e3d7ed4fe7f145
Jorwin Reyes <jorwin2002@gmail.com>
2025-08-02 12:15:08 -0400

    Initialize constants.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,27 @@
 // index.js
+
+// Services provides
+export const servicesData = [];
+
+// Key projects
+export const projects = [];
+
+// Social media links
+export const socials = [
+    {
+        name: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/jorwin-roberto-reyes-191b26257/',
+    },
+    {
+        name: 'GitHub',
+        href: 'https://github.com/Jorwin-dev',
+    },
+    {
+        name: 'Youtube',
+        href: 'https://www.youtube.com/@jorwin2002',
+    },
+    {
+        name: 'Letterboxd',
+        href: 'https://letterboxd.com/Jorwin/',
+    },
+];
