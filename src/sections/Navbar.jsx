c5dc106dc520c7b5e68b11c61551d63bdef55871
Jorwin Reyes <jorwin2002@gmail.com>
2025-08-05 16:17:08 -0400

    Integrate Navbar using gsap and react-scroll animations.

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,12 +1,104 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { socials } from '../constants'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 const navRef = useRef(null);
 const linksRef = useRef([]);
 const contactRef = useRef(null);
+const topLineRef = useRef(null);
+const bottomLineRef = useRef(null);
+const iconTL = useRef(null);
+const tl = useRef(null);
+const [isOpen, setIsOpen] = useState(false);
+const [showBurger, setShowBurger] = useState(true);
+  useGSAP(() => {
+    gsap.set(navRef.current, {xPercent: 100});
+    gsap.set([linksRef.current, contactRef.current], {
+        autoAlpha: 0,
+        x: -20,
+    });
+
+    tl.current = gsap.timeline({paused: true}).to(navRef.current, {
+        xPercent: 0,
+        duration: 1,
+        ease: 'power3.out',
+    })
+    .to(
+        linksRef.current, 
+        {
+            autoAlpha: 1,
+            x: 0,
+            stagger: 0.1,
+            duration: 0.5,
+            ease: 'power2.out',
+        },
+        '<'
+    )
+    .to(
+        contactRef.current, 
+        {
+            autoAlpha: 1,
+            x: 0,
+            duration: 0.5,
+            ease: 'power2.out',
+        },
+        '<+0.2'
+    );
+
+    iconTL.current = gsap
+    .timeline({paused: true})
+    .to(topLineRef.current, {
+        rotate: 45,
+        y: 3.3,
+        duration: 0.3,
+        ease: 'power2.inOut',
+    })
+    .to(
+        bottomLineRef.current, 
+        {
+            rotate: -45,
+            y: -3.3,
+            duration: 0.3,
+            ease: 'power2.inOut',
+        }, 
+        '<'
+    );
+  }, []);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+
+        setShowBurger(currentScrollY <= lastScrollY || 
+        currentScrollY < 10);
+
+        lastScrollY = currentScrollY;
+    };
+    window.addEventListener('scroll', handleScroll, {
+        passive: true,
+    });
+    return () => window.removeEventListener('scroll', 
+        handleScroll);
+  }, []);
+
+  const toggleMenu = () => {
+    if(isOpen){
+        tl.current.reverse();
+        iconTL.current.reverse();
+    }
+    else {
+        tl.current.play();
+        iconTL.current.play();
+    }
+    setIsOpen(!isOpen);
+  }
   return ( 
-    <nav ref={navRef} 
+    <>
+        <nav ref={navRef} 
     className='fixed z-50 flex flex-col justify-between 
     w-full h-full px-10 uppercase bg-black text-white/80 
     py-28 gap-y-10 md:w-1/2 md:left-1/2'
@@ -16,10 +108,16 @@ const contactRef = useRef(null);
             {["home", "services", "about", "work", "contact"].map(
                 (section, index) => (
                     <div key={index} ref={(el) => (linksRef.current[index] = el)}>
-                        <a className='transition-all duration-300 
-                        cursor-pointer hover:text-white'>
+                        <Link 
+                            className='transition-all duration-300 
+                            cursor-pointer hover:text-white'
+                            to={`${section}`}
+                            smooth
+                            offset={0}
+                            duration={2000}
+                        >
                             {section}
-                        </a>
+                        </Link>
                     </div>
                 )
             )}
@@ -57,7 +155,23 @@ const contactRef = useRef(null);
                     </div>
                 </div>
             </div>
-    </nav>
+        </nav>
+        <div className='fixed z-50 flex flex-col items-center
+        justify-center gap-1 transition-all duration-300 bg-black
+        rounded-full cursor-pointer w-14 h-14 md:w-20 md:h-20 top-4
+        right-10' 
+        onClick={toggleMenu}
+        style={showBurger
+            ? { clipPath: 'circle(50.1% at 50% 50%)'}
+            : {clipPath: 'circle(0% at 50% 50%)'}
+        }
+        >
+            <span ref={topLineRef} className='block w-8 h-0.5 bg-white rounded-full
+            origin-center'></span>
+            <span ref={bottomLineRef} className='block w-8 h-0.5 bg-white rounded-full
+            origin-center'></span>
+        </div>
+    </>
   );
 };
 
