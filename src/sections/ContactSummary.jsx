ad8502f6e6a7fc69d95da700e64138ed9eb5a609
Jorwin Reyes <jorwin2002@gmail.com>
2025-08-19 18:52:12 -0400

    Finalize contact summary section

--- src/sections/ContactSummary.jsx
+++ src/sections/ContactSummary.jsx
@@ -1,7 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef } from "react"
-
+import Marquee from "../components/Marquee";
 
 const ContactSummary = () => {
     const containerRef = useRef(null);
@@ -34,10 +34,26 @@ const ContactSummary = () => {
         });
     }, []);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section ref={containerRef} className="flex flex-col 
+    items-center justify-between min-h-screen gap-12 mt-16">
+        <Marquee items={items} />
+        <div className="overflow-hidden font-light text-center contact-text-responsive">
+            <p>
+                " Let's build a <br />
+                <span className="font-normal">memorable</span> &{""}
+                <span className="italic">inspiring</span> <br />
+                web application <span className="text-gold">together</span> "
+            </p>
+        </div>
+        <Marquee 
+            items={items2}
+            reverse={true}
+            className="text-black bg-transparent border-y-2"
+            iconClassName="stroke-gold stroke-2 text-primary"
+            icon="material-symbols-light:square"
+        />
+    </section>
+  );
+};
 
 export default ContactSummary
