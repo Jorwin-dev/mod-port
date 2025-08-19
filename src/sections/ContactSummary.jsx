d7a564fa4a012203049c8a99f8735ddf204bba1f
Jorwin Reyes <jorwin2002@gmail.com>
2025-08-19 18:44:52 -0400

    Create contact summary section and initialize constants

---
+++ src/sections/ContactSummary.jsx
@@ -0,0 +1,27 @@
+import { useRef } from "react"
+
+
+const ContactSummary = () => {
+    const containerRef = useRef(null);
+    const items = [
+        "Innovation",
+        "Precision",
+        "Trust",
+        "Collaboration",
+        "Excellence",
+    ];
+    const items2 = [
+        "contact us",
+        "contact us",
+        "contact us",
+        "contact us",
+        "contact us",
+    ];
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContactSummary
