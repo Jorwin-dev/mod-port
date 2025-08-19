3f8b2bb0f2b71b5a8e1579638210954b482dc913
Jorwin Reyes <jorwin2002@gmail.com>
2025-08-19 15:08:30 -0400

    Integrate services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,10 +1,17 @@
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection"
 
 
 const Services = () => {
+    const text = "I build secure, high-performance full-stack apps\nwith smooth UX to drive growth, not headaches."
   return (
     <section id="services" className="min-h-screen
     bg-black rounded-t-4xl">
-      
+        <AnimatedHeaderSection 
+            subTitle={"Behind the scenes, beyond the screen"}
+            title={"Service"}
+            text={text}
+            textColor={"text-white"}
+        />
     </section>
   )
 }
