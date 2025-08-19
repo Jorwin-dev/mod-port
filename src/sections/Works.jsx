a0409ecbc6fd613a688e9d23cebe93e78b0ed1e6
Jorwin Reyes <jorwin2002@gmail.com>
2025-08-19 18:34:38 -0400

    Finalize works section

--- src/sections/Works.jsx
+++ src/sections/Works.jsx
@@ -1,6 +1,9 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef, useState } from "react"
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { projects } from "../constants";
 
 
 const Works = () => {
@@ -87,15 +90,84 @@ const Works = () => {
         if (window.innerWidth < 768) return;
         mouse.current.x = e.clientX + 24;
         mouse.current.y = e.clientY + 24;
-        mouseX.current(mouse.current.x);
-        mouseY.current(mouse.current.y);
+        moveX.current(mouse.current.x);
+        moveY.current(mouse.current.y);
     };
-    
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id='work' className="flex flex-col min-h-screen">
+        <AnimatedHeaderSection 
+            subTitle={"Logic meets Aesthetics, Seamlessly"}
+            title={"Works"}
+            text={text}
+            textColor={"text-black"}
+            withScrollTrigger={true}
+        />
+        <div className="relative flex flex-col font-light" 
+            onMouseMove={handleMouseMove}
+        >
+            {projects.map((project, index) => (
+                <div
+                    key={project.id}
+                    id="project"
+                    className="relative flex flex-col gap-1 py-5 cursor-pointer group md:gap-0"
+                    onMouseEnter={() => handleMouseEnter(index)}
+                    onMouseLeave={() => handleMouseLeave(index)}
+                >
+                    {/* overlay */}
+                    <div
+                        ref={(el) => {
+                            overlayRefs.current[index] = el;
+                        }}
+                        className="absolute inset-0 hidden md:block duration-200 bg-black -z-10 clip-path"
+                    />
+
+                    {/* title */}
+                    <div className="flex justify-between px-10 text-black transition-all duration-500 md:group-hover:px-12 md:group-hover:text-white">
+                        <h2 className="lg:text-[32px] text-[26px] leading-none">
+                            {project.name}
+                        </h2>
+                        <Icon icon="lucide:arrow-up-right" className="md:size-6 size-5"/>
+                    </div>
+                    {/* divider */}
+                    <div className="w-full h-0.5 bg-black/80"/>
+                    {/* framework */}
+                    <div className="flex px-10 text-xs leading-loose uppercase transition-all duration-500 md:text-sm gap-x-5 md:group-hover:px-12">
+                        {project.frameworks.map((framework) => (
+                            <p key={framework.id} className="text-black transition-colors duration-500 md:group-hover:text-white">
+                                {framework.name}
+                            </p>
+                        ))}
+                    </div>
+                    {/* mobile preview image */}
+                    <div className="relative flex items-center justify-center px-10 md:hidden h-[400px]"> 
+                        <img 
+                            src={project.bgImage}
+                            alt={`${project.name}-big-image`}
+                            className="object-cover w-full h-full rounded-md brightness-50"
+                        />
+                        <img 
+                            src={project.image}
+                            alt={`${project.name}-image`}
+                            className="absolute bg-center px-14 rounded-xl"
+                        />
+                    </div>
+                </div>
+            ))}
+            {/* desktop floating preview image */}
+            <div ref={previewRef} className="fixed -top-2/6 left-0 z-50 overflow-hidden border-black
+            pointer-events-none w-[960px] md:block hidden opacity-0">
+                {currentIndex !== null && (
+                    <img 
+                        src={projects[currentIndex.image]}
+                        alt="preview"
+                        className="object-cover w-full h-full"
+                    />
+                )}
+            </div>
+        </div>
+    </section>
+  );
+};
 
 export default Works
