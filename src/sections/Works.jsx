51020800a3ac56cbf68fffefb8f1a5d2e4688842
Jorwin Reyes <jorwin2002@gmail.com>
2025-08-19 17:13:14 -0400

    Define constants

--- src/sections/Works.jsx
+++ src/sections/Works.jsx
@@ -1,6 +1,16 @@
+import { useRef, useState } from "react"
 
 
 const Works = () => {
+    const overlayRefs = useRef([]);
+    const previewRefs = useRef(null);
+
+    const [currentIndex, setCurrentIndex] = useState(null);
+    const text = "Featured projects that have been meticulously crafted with passion to drive results and impact";
+
+    const mouse = useRef({ x: 0, y: 0 });
+    const moveX = useRef(null);
+    const moveY = useRef(null);
   return (
     <div>
       
