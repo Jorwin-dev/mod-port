7e2b62b447bf011fce91af4a77d7cb415fb4df92
Jorwin Reyes <jorwin2002@gmail.com>
2025-08-19 14:38:48 -0400

    Finalize animations for service summary

--- src/sections/ServiceSummary.jsx
+++ src/sections/ServiceSummary.jsx
@@ -1,8 +1,43 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 const ServiceSummary = () => {
-  return (
+  useGSAP(() => {
+    gsap.to("#title-service-1", {
+        xPercent: 20,
+        scrollTrigger:{
+            target: "#title-service-1",
+            scrub: true,
+        },
+    });
+    gsap.to("#title-service-2", {
+        xPercent: -30,
+        scrollTrigger:{
+            target: "#title-service-2",
+            scrub: true,
+        },
+    });
+    gsap.to("#title-service-3", {
+        xPercent: 100,
+        scrollTrigger:{
+            target: "#title-service-3",
+            scrub: true,
+        },
+    });
+    gsap.to("#title-service-4", {
+        xPercent: -100,
+        scrollTrigger:{
+            target: "#title-service-4",
+            scrub: true,
+        },
+    });
+  });
+    return (
     <section className="mt-20 overflow-hidden font-light
     leading-snug text-center mb-42 contact-text-responsive">
-        <div>
+        <div id="title-service-1">
             <p>Architecture</p>
         </div>
         <div id="title-service-2" className="flex items-center 
