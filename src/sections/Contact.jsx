82a7b9a94991cbf926c01e1b7f42c69a522a41b1
Jorwin Reyes <jorwin2002@gmail.com>
2025-08-19 18:58:39 -0400

    Integrate gsap in contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,3 +1,5 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Contact = () => {
     const text = "Got a question, how or project Idea?\nI'd love to hear from you and discuss further!"
@@ -8,6 +10,19 @@ const Contact = () => {
         "just imagin, I code",
         "just imagin, I code",
     ];
+    useGSAP(() => {
+        gsap.from(".social-link", {
+            y: 100, 
+            opacity: 0,
+            delay: 0.5,
+            duration: 1, 
+            stagger: 0.3,
+            ease: "back.out",
+            scrollTrigger: {
+                trigger: ".social.link",
+            },
+        });
+    }, []);
   return (
     <div>
       
