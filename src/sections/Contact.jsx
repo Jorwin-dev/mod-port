a60743bad74bac5b1ca4aa71952d541323390d15
Jorwin Reyes <jorwin2002@gmail.com>
2025-08-19 18:56:34 -0400

    Define constants in contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,5 +1,13 @@
 
 const Contact = () => {
+    const text = "Got a question, how or project Idea?\nI'd love to hear from you and discuss further!"
+    const items = [
+        "just imagin, I code",
+        "just imagin, I code",
+        "just imagin, I code",
+        "just imagin, I code",
+        "just imagin, I code",
+    ];
   return (
     <div>
       
